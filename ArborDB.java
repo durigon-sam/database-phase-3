3987df99ac3969a35997860574b63a56b3cebdc1
Sam Durigon <smd152@pitt.edu>
2023-12-05 12:01:50 -0500

    create vscode project with JDBC dependency

--- ArborDB.java
+++ ArborDB.java
@@ -13,5 +13,40 @@ public class ArborDB{
             System.err.println("Unable to detect the JDBC .jar dependency. Check that the library is correctly loaded in and try again.");
             System.exit(-1);
         }
+
+        Scanner scanner = new Scanner(System.in);
+        String url = "jdbc:postgresql://localhost:5432/";
+        Properties props = new Properties();
+
+        try {
+            System.out.println("Enter the username for connecting to the database: ");
+            String username = scanner.nextLine();
+            System.out.println("Enter the password for connecting to the database: ");
+            String password = scanner.nextLine();
+
+            props.setProperty("user", username);
+            props.setProperty("password", password);
+            props.setProperty("escapeSyntaxCallMode", "callIfNoReturn");
+
+        } catch (NoSuchElementException ex) {
+            System.err.println("No lines were read from user input, please try again.");
+            System.exit(-1);
+        } catch (IllegalArgumentException ex) {
+            System.err.println("The scanner was likely closed before reading the user's input, please try again.");
+            System.exit(-1);
+        }
+
+        // Connect to the ArborDB database
+        try (Connection conn = DriverManager.getConnection(url, props)){
+            System.out.println("connected to ArborDB");
+        } catch (SQLException err) {
+            System.out.println("SQL Error");
+            while (err != null) {
+                System.out.println("Message = " + err.getMessage());
+                System.out.println("SQLState = " + err.getSQLState());
+                System.out.println("SQL Code = " + err.getErrorCode());
+                err = err.getNextException();
+            }
+        }
     }
 }
